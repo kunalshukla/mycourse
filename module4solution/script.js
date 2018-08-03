f42c3c288d6a443c4ba108c0511feecda3610816
kunalshukla <39904739+kunalshukla@users.noreply.github.com>
2018-08-03 11:43:43 +0530

    Update script.js

--- module4solution/script.js
+++ module4solution/script.js
@@ -34,7 +34,8 @@ WARNING!!! WARNING!!!
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
+var helloSpeaker;
+  var byeSpeaker;
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
